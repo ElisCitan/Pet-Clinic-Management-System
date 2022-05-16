388bebd468f4655ea7f38415aecdee39213bda29
DESKTOP-FFRQOG1\G <andronacheelisabeta@yahoo.com>
2022-05-16 20:00:28 +0300

    PCMS-12 Define hibernate database mode

--- src/main/java/com/sda/eli/petclinic/Main.java
+++ src/main/java/com/sda/eli/petclinic/Main.java
@@ -1,4 +1,11 @@
 package com.sda.eli.petclinic;
 
+import com.sda.eli.petclinic.utils.SessionManager;
+import org.hibernate.Session;
+
 public class Main {
+    public static  void main(String[] arg){
+//temporary
+        SessionManager.getSessionFactory().openSession();
+    }
 }
