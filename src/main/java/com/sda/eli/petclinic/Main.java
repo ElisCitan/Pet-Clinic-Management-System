5ef212f44c0d7cb74d31327e623c180d3bb0f33d
DESKTOP-FFRQOG1\G <andronacheelisabeta@yahoo.com>
2022-05-17 19:17:18 +0300

    PCMS-1 As a user of the system I should be able to add a new veterinarian

--- src/main/java/com/sda/eli/petclinic/Main.java
+++ src/main/java/com/sda/eli/petclinic/Main.java
@@ -1,7 +1,10 @@
 package com.sda.eli.petclinic;
 
 import com.sda.eli.petclinic.controller.VeterinarianController;
+import com.sda.eli.petclinic.option.UserOption;
 import com.sda.eli.petclinic.utils.SessionManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +12,36 @@ public class Main {
         SessionManager.getSessionFactory().openSession();
         SessionManager.getSessionFactory();
         VeterinarianController veterinarianController = new VeterinarianController();
-        veterinarianController.create();
+        //veterinarianController.create();
+        Scanner scanner = new Scanner(System.in);
+
+        UserOption option = UserOption.UNKNOWN;
+        do {
+            UserOption.printAllOption();
+            System.out.println("Choose an option.");
+            String inputOption = scanner.nextLine();
+            try {
+                int userOption = Integer.parseInt(inputOption);
+                option = UserOption.findBy(userOption).orElse(UserOption.UNKNOWN);
+            } catch (NumberFormatException e) {
+                option = UserOption.UNKNOWN;
+            }
+            System.out.println(option.getPrettyName());
+            switch (option){
+                case ADD_NEW_VET:
+                    veterinarianController.create();
+                    break;
+                case UNKNOWN:
+                    break;
+                case EXIT:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("This option is not implemented." );
+                    break;
+            }
+        } while ( !option.equals(UserOption.EXIT));
+        SessionManager.shutdown();
 
     }
 }
